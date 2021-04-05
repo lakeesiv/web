417061348a2d51dd28f9c9cef574c64fe619fd3a
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-05 23:18:09 +0100

    passing active page var

--- app.py
+++ app.py
@@ -7,22 +7,25 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", title="Home")
+    return render_template("home.html", title="Home", active_page="home")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="About")
+    return render_template("about.html", title="About", active_page="about")
 
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html", title="Projects")
+    return render_template(
+        "projects.html",
+        title="Projects",
+        active_page="projects")
 
 
 @app.route("/blog")
 def blog():
-    return render_template("blog.html", title="Blog")
+    return render_template("blog.html", title="Blog", active_page="blog")
 
 
 if __name__ == "__main__":
