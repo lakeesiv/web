bd42bc8762623864f3ca6ebf2e358a216424dbc6
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-05 19:14:30 +0100

    formatting

--- app.py
+++ app.py
@@ -7,22 +7,22 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", title = "Home")
+    return render_template("home.html", title="Home")
+
 
 @app.route("/about")
 def about():
-     return render_template("about.html", title = "About")
+    return render_template("about.html", title="About")
+
 
 @app.route("/projects")
 def projects():
-     return render_template("projects.html", title = "Projects")
+    return render_template("projects.html", title="Projects")
+
 
 @app.route("/blog")
 def blog():
-     return render_template("blog.html", title = "Blog")
-
-
-
+    return render_template("blog.html", title="Blog")
 
 
 if __name__ == "__main__":
