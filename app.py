5ef4ea2c287bc58b119d549c7d2d9d11322090f2
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-05 15:26:29 +0100

    added more pages

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return "Home"
+    return render_template("home.html", title = "Test")
+
+@app.route("/about")
+def about():
+    return "about"
+
+@app.route("/projects")
+def projects():
+    return "projects"
+
+@app.route("/blog")
+def blog():
+    return "blog"
+
+
+
 
 
 if __name__ == "__main__":
