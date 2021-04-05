fc7da831cffd49d713c17d1309202b4fd87c0b00
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-05 23:55:48 +0100

    404 routing

--- app.py
+++ app.py
@@ -28,5 +28,10 @@ def blog():
     return render_template("blog.html", title="Blog", active_page="blog")
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(debug=True)
