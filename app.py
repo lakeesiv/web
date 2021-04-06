6f072437dcdb79831106088d0e152dd82aab9aba
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-06 15:36:12 +0100

    added contact page

--- app.py
+++ app.py
@@ -40,6 +40,14 @@ def blog():
     return render_template("blog.html", title="Blog", active_page="blog")
 
 
+@app.route("/contact")
+def contact():
+    return render_template(
+        "contact.html",
+        title="Contact",
+        active_page="contact")
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
