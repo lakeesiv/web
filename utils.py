1691d6ed7be49126ba5c86f3bc4bb53c39959296
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-06 16:56:26 +0100

    added email checker

--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
+import re
 
 
 def get_secret_key():
     f = open("secret_key.txt", "r")
     return f.read()
+
+
+def email_check(email):
+    EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+    if EMAIL_REGEX.match(email):
+        return True
+    else:
+        return False
