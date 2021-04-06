3a8c665df3275d2dcb734a6b476a3024897c17f2
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-06 16:27:28 +0100

    dealing with POST request

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, redirect, url_for, flash
+from utils import get_secret_key
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.secret_key = get_secret_key()
 
 
 @app.route('/favicon.ico')
@@ -40,17 +42,40 @@ def blog():
     return render_template("blog.html", title="Blog", active_page="blog")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template(
-        "contact.html",
-        title="Contact",
-        active_page="contact")
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["message"]
+
+        if name and email and subject and message:
+            res = name + email + subject + message
+            return redirect(url_for("test", tst=res))
+        else:
+            flash("Make sure there is no empty fields")
+
+        return render_template(
+            "contact.html",
+            title="Contact",
+            active_page="contact")
+
+    else:
+        return render_template(
+            "contact.html",
+            title="Contact",
+            active_page="contact")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@app.route("/<tst>")
+def test(tst):
+    return f"<h1>{tst}</h1>"
 
 
 if __name__ == "__main__":
