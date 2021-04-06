3353546a5e67854099e741c8ccdac4a8911cd142
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-06 13:09:19 +0100

    formatting

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def home():
 
 @app.route("/about")
 def about():
+
     return render_template("about.html", title="About", active_page="about")
 
 
--- static/home.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".home-title", {
-    strings: ["Welcome", "to", "my","Website"],
-    typeSpeed: 50,
-    loop: false,
-    backDelay: 900,
-    backSpeed: 30,
-});
