287cfab3bc06ddf54f6e26e01c954c12b2282d1e
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-06 13:26:21 +0100

    added favicon

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(
+            app.root_path,
+            'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon')
 
 
 @app.route("/")
