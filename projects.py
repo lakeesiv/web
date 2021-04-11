c4726ad6e22552ff8c747a96c7f4bb0ba83a7518
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-11 14:26:08 +0100

    added projects page

--- projects.py
+++ projects.py
@@ -9,6 +9,8 @@ projects = Blueprint(
     static_folder='projects/static',
 )
 
+projects_data = projects_json("projects/projects.json")
+
 
 @projects.route("/")
 def projects_home():
@@ -16,4 +18,19 @@ def projects_home():
         "projects.html",
         title="Projects",
         active_page="projects",
-        projects=projects_json("projects/projects.json"))
+        projects=projects_data)
+
+
+@projects.route("/<string:project_link>")
+def projects_page(project_link):
+    links = [d["link"] for d in projects_data]
+    if project_link in links:
+        for d in projects_data:
+            if project_link == d["link"]:
+                title = d["title"]
+        return render_template(
+            f"projects/{project_link}.html",
+            title=f"{title}",
+            active_page="projects")
+    else:
+        return render_template("404.html"), 404
