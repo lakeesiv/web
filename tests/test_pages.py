6b3148b6375bd701e1190607b31bfec11623fff9
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-15 14:52:18 +0100

    project pages test

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -8,3 +8,11 @@ def test_pages(app, client):
         res = client.get(f"/{page}")
         assert res.status_code == 200 or 308
 
+def test_project_pages(app, client):
+    del app
+    pages = ["etoh","flask-website","x-ray-cnn"]
+
+    for page in pages:
+        print(page)
+        res = client.get(f"projects/{page}")
+        assert res.status_code == 200 or 308
