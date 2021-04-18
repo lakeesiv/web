7a511d51c0fedc4be0cb293846c2c7cd9847b0e1
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-04-18 15:37:50 +0100

    removed uncessary funcs

--- utils.py
+++ utils.py
@@ -11,22 +11,6 @@ def env(var):
     return os.environ.get(var)
 
 
-def get_secret_key():
-    return env("SECRET_KEY")
-
-
-def get_api_key():
-    return env("SENDGRID_API_KEY")
-
-
-def get_sender():
-    return env("SENDER")
-
-
-def get_reciever():
-    return env("RECIEVER")
-
-
 def email_check(email):
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
     if EMAIL_REGEX.match(email):
@@ -39,7 +23,3 @@ def get_json(path):
     with open(path) as json_file:
         data = json.load(json_file)
         return data
-
-
-def get_status():
-    return env("STATUS")
